32edac065c8c5ba2ce9404178bb8f624aab8ed14
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-11 00:22:21 -0400

    add js

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,69 @@
 const rulesBtn = document.getElementById("rules-btn");
 const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let score = 0;
+
+//Create ball props
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+// Create paddle props
+
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+
+//Draw ball on canvas
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true);
+  ctx.fillStyle = "#183D3D";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw paddle on canvas
+
+function draPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#183D3D";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw everything
+
+function draw() {
+  drawBall();
+  draPaddle();
+  drawScore();
+}
+
+// Draw score on canvas
+
+function drawScore() {
+  ctx.font = '20px Poppins';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+draw();
 
 // Rules and close event handers
 
