e2f18c0872f0b6befb39642896afca6800335d3e
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-10-14 14:19:08 +0330

    add js

--- Script/script.js
+++ Script/script.js
@@ -9,7 +9,7 @@ let score = 0;
 const brickRowCount = 9;
 const brickColumnCount = 5;
 
-//Create ball props
+// Create ball props
 
 const ball = {
   x: canvas.width / 2,
@@ -38,12 +38,11 @@ const brickInfo = {
   h: 20,
   padding: 10,
   offsetX: 45,
-  offsetY: 65,
+  offsetY: 60,
   visible: true,
 };
 
-//Create brick
-
+// Create bricks
 const bricks = [];
 for (let i = 0; i < brickRowCount; i++) {
   bricks[i] = [];
@@ -54,48 +53,48 @@ for (let i = 0; i < brickRowCount; i++) {
   }
 }
 
-//Draw ball on canvas
+// Draw ball on canvas
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true);
-  ctx.fillStyle = "#183D3D";
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#092020";
   ctx.fill();
   ctx.closePath();
 }
 
 // Draw paddle on canvas
 
-function draPaddle() {
+function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-  ctx.fillStyle = "#183D3D";
+  ctx.fillStyle = "#092020";
   ctx.fill();
   ctx.closePath();
 }
 
-// Draw score on canvas
+// Draw score oon canvas
 
 function drawScore() {
-  ctx.font = "20px Poppins";
+  ctx.font = "20px Arial";
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
-//Draw bricks on canvas
+// Draw bricks on canvas
 
 function drawBricks() {
   bricks.forEach((column) => {
     column.forEach((brick) => {
       ctx.beginPath();
       ctx.rect(brick.x, brick.y, brick.w, brick.h);
-      ctx.fillStyle = brick.visible ? " #5c83555" : "transparent";
+      ctx.fillStyle = brick.visible ? "#173a3a" : "Poppins";
       ctx.fill();
       ctx.closePath();
     });
   });
 }
 
-// Move padd on canvas
+// Move paddle on canvas
 
 function movePaddle() {
   paddle.x += paddle.dx;
@@ -116,17 +115,19 @@ function moveBall() {
   ball.x += ball.dx;
   ball.y += ball.dy;
 
-  // Wall collision (right\left)
+  // Wall collision (right/left)
   if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
-    ball.dx *= -1;
+    ball.dx *= -1; // ball.dx = ball.dx * -1
   }
 
-  // Wall collision (bottom\top)
+  // Wall collision (top/bottom)
   if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
     ball.dy *= -1;
   }
 
-  // paddle collision
+  // console.log(ball.x, ball.y);
+
+  // Paddle collision
   if (
     ball.x - ball.size > paddle.x &&
     ball.x + ball.size < paddle.x + paddle.w &&
@@ -136,41 +137,70 @@ function moveBall() {
   }
 
   // Brick collision
+
   bricks.forEach((column) => {
     column.forEach((brick) => {
       if (brick.visible) {
         if (
-          ball.x - ball.size > brick.x &&
-          ball.x + ball.size < brick.x + brick.w &&
-          ball.y + ball.size > brick.y &&
-          ball.y - ball.size < brick.y + brick.h
+          ball.x - ball.size > brick.x &&   // left brick side check
+          ball.x + ball.size < brick.x + brick.w &&    // right brick side check
+          ball.y + ball.size > brick.y &&     // top brick side check
+          ball.y - ball.size < brick.y + brick.h    // bottom brick side check
         ) {
           ball.dy *= -1;
           brick.visible = false;
+
+          increaseScore();
         }
       }
     });
+  });
+
+  // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+}
+
+// Increase score
+
+function increaseScore() {
+  score++;
+
+  if (score % (brickRowCount * brickRowCount) === 0) {
+    showAllBricks();
+  }
+}
+
+// Make all bricks appear
+
+function showAllBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => (brick.visible = true));
   });
 }
 
 // Draw everything
 
 function draw() {
+  // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawBall();
-  draPaddle();
+  drawPaddle();
   drawScore();
   drawBricks();
 }
 
-// Update camvas drawing and animation
+// Update canvas drawing and animation
 
 function update() {
   movePaddle();
   moveBall();
 
-  //Draw everything
+  // Draw everything
+
   draw();
 
   requestAnimationFrame(update);
@@ -180,7 +210,7 @@ update();
 
 // Keydown event
 
-function keydown(e) {
+function keyDown(e) {
   if (e.key === "Right" || e.key === "ArrowRight") {
     paddle.dx = paddle.speed;
   } else if (e.key === "Left" || e.key === "ArrowLeft") {
@@ -188,7 +218,7 @@ function keydown(e) {
   }
 }
 
-// keyUp event
+// Keyup event
 
 function keyUp(e) {
   if (
@@ -201,11 +231,13 @@ function keyUp(e) {
   }
 }
 
-//Keyboard event handlers
-document.addEventListener("keydown", keydown);
+// Keyboard event handlers
+
+document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
 
-// Rules and close event handers
+// Rules and close event handlers
 
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
+إ
